af1ed2588eb946fe735f6c815dc6f02f18757456
Justin Reeves <reeves122@gmail.com>
2023-11-27 15:52:15 -0600

    test ipam module

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "public_subnets_ids" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet#id"
-  value       = module.vpc.public_subnets_ids
+  value       = module.vpc[0].public_subnets_ids
 }
 
 output "private_subnets_ids" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet#id"
-  value       = module.vpc.private_subnets_ids
+  value       = module.vpc[0].private_subnets_ids
 }
 
 output "vpc_id" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group#vpc_id"
-  value       = module.vpc.vpc_id
+  value       = module.vpc[0].vpc_id
 }
 
 output "transit_gateway_vpc_attachment_id" {
@@ -20,5 +20,5 @@ output "transit_gateway_vpc_attachment_id" {
 
 output "cidr_block" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc#cidr_block"
-  value       = module.vpc.cidr_block
+  value       = module.vpc[0].cidr_block
 }
