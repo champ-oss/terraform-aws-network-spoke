2a1cff8f286df8ecfa7aa8c24b0776a1da85692d
Justin Reeves <reeves122@gmail.com>
2023-11-28 10:14:59 -0600

    test ipam module

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,34 +1,3 @@
-#resource "aws_vpc_ipam" "this" {
-#  count = 1
-#
-#  operating_regions {
-#    region_name = "us-east-2"
-#  }
-#}
-#
-#resource "aws_vpc_ipam_scope" "this" {
-#  count   = 1
-#  ipam_id = aws_vpc_ipam.this[0].id
-#}
-#
-#resource "aws_vpc_ipam_pool" "this" {
-#  count                             = 1
-#  ipam_scope_id                     = aws_vpc_ipam_scope.this[0].id
-#  source_ipam_pool_id               = aws_vpc_ipam_pool.this[0].id
-#  address_family                    = "ipv4"
-#  locale                            = "us-east-2"
-#  allocation_default_netmask_length = 18
-#  allocation_min_netmask_length     = 18
-#  allocation_max_netmask_length     = 18
-#}
-#
-#resource "aws_vpc_ipam_pool_cidr" "this" {
-#  count          = 1
-#  ipam_pool_id   = aws_vpc_ipam_pool.this[0].id
-#  netmask_length = 12
-#  cidr           = "10.0.0.0/8"
-#}
-
 module "ipam" {
   source   = "github.com/aws-ia/terraform-aws-ipam?ref=v2.1.0"
   count    = 1
@@ -36,8 +5,8 @@ module "ipam" {
   top_name = "global"
 
   pool_configurations = {
-    us-east-2 = {
-      description    = "us-east-2"
+    terraform-aws-network-spoke = {
+      locale         = "us-east-2"
       netmask_length = 9
     }
   }
@@ -49,7 +18,8 @@ resource "aws_ec2_transit_gateway" "this" {
 }
 
 module "this" {
-  source             = "../../"
-  ipv4_ipam_pool_id  = module.ipam[0].pools_level_1["us-east-2"].id
-  transit_gateway_id = aws_ec2_transit_gateway.this[0].id
+  source = "../../"
+  #ipv4_ipam_pool_id  = module.ipam[0].pools_level_1["us-east-2"].id
+  ipam_search_description = "terraform-aws-network-spoke"
+  transit_gateway_id      = aws_ec2_transit_gateway.this[0].id
 }
