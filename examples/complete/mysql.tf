8f5a6348196cf4ca383731a5a28396ab814a5ced
Phillip Langley <philliplangley@champmbplangley.lan>
2024-11-21 13:49:27 -0600

    test spoke with it disabled

--- examples/complete/mysql.tf
+++ examples/complete/mysql.tf
@@ -1,7 +1,7 @@
 # Create test database in the hub VPC to test transit gateway connectivity
 module "mysql" {
   source                   = "github.com/champ-oss/terraform-aws-mysql.git?ref=v1.0.170-72f9097"
-  enabled                  = var.enabled
+  count                    = var.enabled ? 1 : 0
   name_prefix              = "network-spoke"
   source_security_group_id = aws_security_group.test[0].id
   vpc_id                   = module.hub[0].vpc_attributes.id
